4548228180144c60038daf47704f28d6c2103fd3
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-09 10:29:41 +0900

    サンプルコードへ移植

--- ChromaCqt.py
+++ ChromaCqt.py
@@ -92,17 +92,3 @@ class ChromaCqt():
         self.chromacqt_execute(self.hpss_data,chroma_mode,hop_length,n_octaves,n_chroma)
         '''
 
-    
-#実装サンプル
-al1=ChromaCqt()
-al1.load_music('file.wav')
-#al1.cut(al1.loaded_data,7,56)
-#al1.lr_separate(al1.cut_data)
-al1.lr_separate(al1.loaded_data)
-al1.hpss_execute(al1.lr_separated_data,'right')
-al1.chromacqt_execute(al1.hpss_data['harmonic'])
-
-print(al1.chrcqt_data)
-al1.disp_chrcqt(al1.chrcqt_data)
-al1.export_csv()
-

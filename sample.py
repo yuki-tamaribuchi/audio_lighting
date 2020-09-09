548fa06ea64b7c139a376a3f71baf9347e73929c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-09 10:32:22 +0900

    インスタンス名変更

--- sample.py
+++ sample.py
@@ -1,14 +1,14 @@
 from ChromaCqt import ChromaCqt
 
 #実装サンプル
-al1=ChromaCqt()
-al1.load_music('file.wav')
-#al1.cut(al1.loaded_data,7,56)
-#al1.lr_separate(al1.cut_data)
-al1.lr_separate(al1.loaded_data)
-al1.hpss_execute(al1.lr_separated_data,'right')
-al1.chromacqt_execute(al1.hpss_data['harmonic'])
+cc1=ChromaCqt()
+cc1.load_music('file.wav')
+#cc1.cut(cc1.loaded_data,7,56)
+#cc1.lr_separate(cc1.cut_data)
+cc1.lr_separate(cc1.loaded_data)
+cc1.hpss_execute(cc1.lr_separated_data,'right')
+cc1.chromacqt_execute(cc1.hpss_data['harmonic'])
 
-print(al1.chrcqt_data)
-al1.disp_chrcqt(al1.chrcqt_data)
+print(cc1.chrcqt_data)
+cc1.disp_chrcqt(cc1.chrcqt_data)
 al1.export_csv()

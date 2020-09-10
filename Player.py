87fe64d02591c2964ce69d269cfb63de4872a326
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-10 11:52:59 +0900

    再生用関数等の追加(途中)

--- Player.py
+++ Player.py
@@ -1,10 +1,13 @@
-
-
+import numpy as np
+import sounddevice
 
 class Player():
-    def __init__(self):
-        pass
+
+    def __init__(self,audio_data,rate,lighting_data=None):
+        self.audio_data=audio_data
+        self.rate=rate
+        self.lighting_data=lighting_data
 
 
-    def play(self,audio_data,lighting_data):
-        
+    def play_w_lightring(self):
+        sounddevice.play(data=self.audio_data,rate=self.rate)

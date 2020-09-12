7276463e32671f9e607d27a7dc2755ac23ff389f
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-12 17:32:19 +0900

    仕様変更に伴う修正

--- sample.py
+++ sample.py
@@ -1,12 +1,6 @@
 from ChromaCqt import ChromaCqt
 
 #実装サンプル
-cc1=ChromaCqt()
-cc1.load_music('file.wav')
-cc1.lr_separate(cc1.normalized_data)
-cc1.hpss_execute(cc1.lr_separated_data)
-cc1.chromacqt_execute_full(cc1.hpss_data)
-#print(cc1.chrcqt_data)
-#cc1.disp_chrcqt(cc1.chrcqt_data)
+cc1=ChromaCqt('file.wav')
 #cc1.export_csv()
 #cc1.create_io_array()

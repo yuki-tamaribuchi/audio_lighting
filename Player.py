375c8a40efc88b1f7c7f5dc8ba4df298a6dc38d3
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-12 20:20:51 +0900

    信号をprintする関数を追加

--- Player.py
+++ Player.py
@@ -4,15 +4,30 @@ import time
 
 class Player():
 
-    def __init__(self,audio_data,rate,lighting_data=None):
+    def __init__(self,audio_data,rate,lighting_data):
         self.audio_data=audio_data
         self.rate=rate
         self.lighting_data=lighting_data
 
 
+
     def play_w_lightring(self):
         sounddevice.play(self.audio_data,self.rate)
-        audio_len=len(self.audio_data)/self.rate
-        print('Audio Length=',round(audio_len),'sec')
-        time.sleep(audio_len)
+        self.audio_len=len(self.audio_data)/self.rate
+        print('Audio Length=',round(self.audio_len),'sec')
+        self.print_array(self.lighting_data)
+        time.sleep(self.audio_len)
+
+
+    def print_array(self,lighting_data):
+        array_len=len(lighting_data['left'][0])
+        print(array_len)
+        interval=1/(array_len/self.audio_len)
+
+        print(interval)
+
+        for i in range(0,array_len,1):
+            print(lighting_data['left'][0][i],',',lighting_data['right'][0][i])
+            time.sleep(interval)
+
 

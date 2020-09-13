44e15d5705749f47835aceeccdf14caf39604c84
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-14 08:33:20 +0900

    コード変更に伴う修正

--- sample.py
+++ sample.py
@@ -5,6 +5,6 @@ import time
 #実装サンプル
 cc1=ChromaCqt('file.wav')
 
-pl1=Player(cc1.loaded_data[:,0],cc1.rate,cc1.io_array)
+pl1=Player(cc1.loaded_data[:,0],cc1.rate,cc1.chroma_array_left)
 pl1.play_w_lightring()
 

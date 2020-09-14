d491c02546643db044ae259eab0a39935608759c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-14 21:54:42 +0900

    コンストラクタにコネクションを張る処理と，ライトの輝度を127に設定する関数の追加

--- Lighting.py
+++ Lighting.py
@@ -1,7 +1,12 @@
-import phue
+from phue import Bridge
 
 
 class Lighting():
 
+    def __init__(self,ip_addr):
+        b=Bridge(ip_addr)
+        b.connect()
+        self.lights=b.lights
+
     def brightness(self,data):
-        pass
+        self.lights[0]=127

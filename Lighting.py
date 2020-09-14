90ad8e5e8da6e9e74409072228eeb08563b257a2
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-15 00:25:38 +0900

    ライティング用の処理を追加

--- Lighting.py
+++ Lighting.py
@@ -1,5 +1,5 @@
 from phue import Bridge
-
+import time
 
 class Lighting():
 
@@ -7,6 +7,14 @@ class Lighting():
         b=Bridge(ip_addr)
         b.connect()
         self.lights=b.lights
-
-    def brightness(self,data=None):
         self.lights[0].brightness=127
+
+    def brightness(self,data,audio_len):
+        data_length=len(data)
+        print(data_length)
+        interval=audio_len/data_length
+        print(interval)
+        for i in range(0,data_length):
+            self.lights[0].brightness=127+(int(data[i])*30)
+            print(data[i])
+            time.sleep(interval)

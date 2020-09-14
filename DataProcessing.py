8390c187073a4d1e7d5dfc85aa0630c91dfe5119
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-14 13:16:21 +0900

    brightnessデータ生成用関数の追加

--- DataProcessing.py
+++ DataProcessing.py
@@ -19,6 +19,8 @@ class DataProcessing():
     rate=0
     chroma_array_left=[]
     chroma_array_rihgt=[]
+    brightness_left=[]
+    brightness_right=[]
 
 
     def __init__(self,file):
@@ -65,5 +67,17 @@ class DataProcessing():
     def create_io_array(self):
         self.chroma_array_left=np.where(self.chrcqt_left_data==1.0,1,0)
         self.chroma_array_left=np.where(self.chrcqt_right_data==1.0,1,0)
+
+
+    def create_brightness_data(self):
+        left_ave=abs(np.average(self.hpss_percussion_left))
+        right_ave=abs(np.average(self.hpss_percussion_right))
+
+        print(left_ave)
+        print(right_ave)
+        
+
+        self.brightness_left=np.where(self.hpss_percussion_left>left_ave,1,0)
+        self.brightness_right=np.where(self.hpss_harmonics_right>right_ave,1,0)
 
         

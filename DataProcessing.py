1136dd2d6f4fce9ca37cec8b65d1eb083d64fc86
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-21 00:00:59 +0900

    スケールからRGBを通してCIE1931XYへ変換する関数の追加 スペルミスの修正

--- DataProcessing.py
+++ DataProcessing.py
@@ -18,7 +18,7 @@ class DataProcessing():
     chrcqt_right_data=[]
     rate=0
     chroma_array_left=[]
-    chroma_array_rihgt=[]
+    chroma_array_right=[]
     brightness_left=[]
     brightness_right=[]
 
@@ -89,4 +89,112 @@ class DataProcessing():
         
 
     def create_color_data(self):
-        pass
+
+        #convert sRGB to CIE1931 XY
+
+        chroma_rgb={
+            #C
+            0:{
+                'R':50,
+                'G':0,
+                'B':0,
+            },
+            #C#
+            1:{
+                'R':150,
+                'G':150,
+                'B':150,
+            },
+            #D
+            2:{
+                'R':200,
+                'G':190,
+                'B':100,
+            },
+            #D#
+            3:{
+                'R':0,
+                'G':100,
+                'B':65,
+            },
+            #E
+            4:{
+                'R':0,
+                'G':130,
+                'B':65,
+            },
+            #F
+            5:{
+                'R':200,
+                'G':0,
+                'B':30,
+            },
+            #F#
+            6:{
+                'R':230,
+                'G':190,
+                'B':190,
+            },
+            #G
+            7:{
+                'R':255,
+                'G':255,
+                'B':190,
+            },
+            #G#
+            8:{
+                'R':240,
+                'G':240,
+                'B':240,
+            },
+            #A
+            9:{
+                'R':150,
+                'G':100,
+                'B':50,
+            },
+            #A#
+            10:{
+                'R':120,
+                'G':0,
+                'B':130,
+            },
+            #B
+            11:{
+                'R':230,
+                'G':150,
+                'B':190,
+            }
+        }
+
+        rgb_left=chroma_rgb[self.chroma_array_left]
+        rgb_right=chroma_rgb[self.chroma_array_right]
+
+
+        # gamma correction
+        red_left = pow(((rgb_left['R']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_left['R']/256) > 0.04045 else ((rgb_left['R']/256) / 12.92)
+        green_left = pow(((rgb_left['G']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_left['G']/256) > 0.04045 else ((rgb_left['G']/256) / 12.92)
+        blue_left =  pow(((rgb_left['B']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_left['B']/256) > 0.04045 else ((rgb_left['B']/256) / 12.92)
+
+        red_right = pow(((rgb_right['R']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_right['R']/256) > 0.04045 else ((rgb_right['R']/256) / 12.92)
+        green_right = pow(((rgb_right['G']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_right['G']/256) > 0.04045 else ((rgb_right['G']/256) / 12.92)
+        blue_right =  pow(((rgb_right['B']/256) + 0.055) / (1.0 + 0.055), 2.4) if (rgb_right['B']/256) > 0.04045 else ((rgb_right['B']/256) / 12.92)
+
+
+        # convert rgb to xyz
+        x_left = red_left * 0.649926 + green_left * 0.103455 + blue_left * 0.197109
+        y_left = red_left * 0.234327 + green_left * 0.743075 + blue_left * 0.022598
+        z_left = green_left * 0.053077 + blue_left * 1.035763
+
+        x_right = red_right * 0.649926 + green_right * 0.103455 + blue_right * 0.197109
+        y_right = red_right * 0.234327 + green_right * 0.743075 + blue_right * 0.022598
+        z_right = green_right * 0.053077 + blue_right * 1.035763
+
+
+        # convert xyz to xy
+        self.x_left = x_left / (x_left + y_left + z_left)
+        self.y_left = y_left / (x_left + y_left + z_left)
+        
+        self.x_right = x_right / (x_right + y_right + z_right)
+        self.y_right = y_right / (x_right + y_right + z_right)
+    

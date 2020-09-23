2706b1a81b602450f9201ee35c9d1cc33fd1aabf
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-23 10:57:54 +0900

    IPアドレスを固定したため修正 color関数の呼び出し

--- Player.py
+++ Player.py
@@ -10,14 +10,15 @@ class Player():
         self.audio_data=audio_data
         self.rate=rate
         self.lighting_data=lighting_data
-        #self.lt1=Lighting('192.168.11.11')
+        self.lt1=Lighting('192.168.11.99')
 
 
-    def play_w_lightring(self,data):
+    def play_w_lightring(self,brightness_data,color_data):
         sounddevice.play(self.audio_data,self.rate)
         self.audio_len=len(self.audio_data)/self.rate
         print('Audio Length=',round(self.audio_len),'sec')
-        #self.lt1.brightness(data,self.audio_len)
+        #self.lt1.brightness(brightness_data,self.audio_len)
+        self.lt1.color(color_data,self.audio_len)
         time.sleep(self.audio_len)
 
 

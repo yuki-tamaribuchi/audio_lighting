17c5ea2d2bf439da17ff081f3aa346d6fee21d62
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-09-24 21:18:09 +0900

    仕様変更に伴う修正

--- sample.py
+++ sample.py
@@ -4,9 +4,11 @@ import time
 
 #実装サンプル
 
-#audio->'a',video->'v'
-dp1=DataProcessing('file.wav','a')
-dp1.create_brightness_data()
+file='file.mp4'
 
-pl1=Player(dp1.loaded_data[:,0],dp1.rate,dp1.chroma_array_left)
-pl1.play_w_lightring(brightness_data=dp1.brightness_left,color_data=dp1.color_x_left)
+#audio->'a',video->'v'
+dp1=DataProcessing(file,'v')
+#dp1.create_brightness_data()
+
+pl1=Player(file=file,audio_len=dp1.audio_time_length,color_data=dp1.color_x_left)
+pl1.play_w_lightring()

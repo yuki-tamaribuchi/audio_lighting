e29182285154cab5922f0a36214fa3e1c21daabf
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-01 21:23:21 +0900

    仕様変更に伴う修正

--- sample.py
+++ sample.py
@@ -10,5 +10,5 @@ file='media_file/file.mp4'
 #audio->'a',video->'v'
 dp1=DataProcessing(file,'v')
 
-pl1=Player(file=file,audio_time_length=dp1.audio_time_length,color_data=dp1.color_array,brightness_data=dp1.brightness,brightness_v_data=dp1.brightness_from_video)
+pl1=Player(file=file,audio_time_length=dp1.audio_time_length,color_data=dp1.color_array,brightness_data=dp1.brightness,brightness_v_data=None)
 pl1.play_w_lightring()

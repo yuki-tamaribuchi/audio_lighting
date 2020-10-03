500c5a0c8fac5460e0b3693ce2fc054726749449
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-03 15:09:42 +0900

    テスト確認用の変更

--- sample.py
+++ sample.py
@@ -11,4 +11,4 @@ file='media_file/file.mp4'
 dp1=DataProcessing(file,'v')
 
 pl1=Player(file=file,audio_time_length=dp1.audio_time_length,color_data=dp1.color_array,brightness_data=dp1.brightness,brightness_v_data=None)
-pl1.play_w_lightring()
+#pl1.play_w_lightring()

10dffaa24875edb5f21e63a8d6857443a3b6e46c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-04 09:51:11 +0900

    strからfloatに変換する処理を追加

--- DataProcessing.py
+++ DataProcessing.py
@@ -327,7 +327,7 @@ class DataProcessing():
         with open('temp_data/color.csv','r') as f:
             reader=csv.reader(f,delimiter=',')
             for row in reader:
-                self.xy.append(row)
+                self.xy.append([float(s) for s in row])
         self.xy=np.array(self.xy)
         print('Load Color Data from CSV End')
 

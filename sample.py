5e1d21a4fb3ece0b5cd7a556db37b783026d64a9
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-08 20:13:04 +0900

    仕様変更に伴う修正

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 from DataProcessing import DataProcessing
 from Player import Player
+from Lighting import Lighting
 import time
 
 
@@ -9,6 +10,7 @@ file='media_file/rojo.mp4'
 
 #audio->'a',video->'v'
 dp1=DataProcessing(file,'v',True)
+lt1=Lighting('192.168.11.99')
 
-pl1=Player(file,dp1)
+pl1=Player(file,dp1,lt1)
 pl1.play()

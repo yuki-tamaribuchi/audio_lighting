9f195b0cf85264e430b6d757588d8b44bb0dab0c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-15 00:53:17 +0900

    テスト用変更

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 from DataProcessing import DataProcessing
+from PaperData import PaperData
 from Player import Player
 from Lighting import Lighting
 import time
@@ -10,7 +11,13 @@ file='media_file/rojo.mp4'
 
 #audio->'a',video->'v'
 dp1=DataProcessing(file,'v',True)
-lt1=Lighting('192.168.11.99')
 
-pl1=Player(file,dp1,lt1)
+pd=PaperData(dp1)
+pd.create_chroma_array()
+pd.save_chroma_array()
+
+
+#lt1=Lighting('192.168.11.99')
+
+#pl1=Player(file,dp1,lt1)
 #pl1.play()

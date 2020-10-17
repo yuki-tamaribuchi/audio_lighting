e21ce163cbd7225920308ecc3c7f6a04775f6f9b
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-17 11:46:10 +0900

    テスト確認

--- sample.py
+++ sample.py
@@ -6,11 +6,11 @@ import time
 
 #実装サンプル
 
-file='media_file/rojo.mp4'
+file='media_file/piano_only.mp4'
 
 #audio->'a',video->'v'
-dp1=DataProcessing(file,'v',True)
+dp1=DataProcessing(file,'a',True)
 lt1=Lighting('192.168.11.99')
 
 pl1=Player(file,dp1,lt1)
-#pl1.play()
+pl1.play()

15b8046d639dae6d9c2944f9c1d08a33e38c2d4b
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-18 11:58:28 +0900

    テスト変更

--- sample.py
+++ sample.py
@@ -6,11 +6,11 @@ import time
 
 #実装サンプル
 
-file='media_file/piano_only.wav'
+file='media_file/piano_file/piano_only_bpm100_original.wav'
 
 #audio->'a',video->'v'
 dp1=DataProcessing(file,'a',True)
 lt1=Lighting('192.168.11.99')
 
 pl1=Player(file,dp1,lt1)
-#pl1.play()
+pl1.play()
